19e0f74049877e8ccdc5bc35710d6edd7d4c546c
oscarabilleira <oscarabilleira@gmail.com>
2015-07-06 15:09:41 +0200

    Update Viewport.cs

--- src/Viewport.cs
+++ src/Viewport.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.Windows;
 using Windows.Graphics.Display;
-using System.DisplayInformation;
 using System.Windows.Media;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
