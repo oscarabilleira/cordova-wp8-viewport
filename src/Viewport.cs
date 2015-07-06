46433e89573eb9b8680c78319cd67b60dae35455
oscarabilleira <oscarabilleira@gmail.com>
2015-07-06 17:59:04 +0200

    Update Viewport.cs

--- src/Viewport.cs
+++ src/Viewport.cs
@@ -4,7 +4,7 @@ using System.IO.IsolatedStorage;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.Windows;
-using System.Display;
+using Windows.Graphics.Display
 using System.Windows.Media;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
