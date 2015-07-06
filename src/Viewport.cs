ab0add43a0fa5ef3b98ecf1dd842e2afeabd8551
oscarabilleira <oscarabilleira@gmail.com>
2015-07-06 22:13:08 +0200

    Update Viewport.cs

--- src/Viewport.cs
+++ src/Viewport.cs
@@ -1,34 +1,19 @@
 using System;
-using System.Collections.Generic;
-using System.IO.IsolatedStorage;
-using System.Linq;
-using System.Text;
-using System.Runtime.Serialization;
-using System.Windows;
-using Windows.Graphics.Display;
-using System.Windows.Media;
-using Microsoft.Phone.Controls;
-using Microsoft.Phone.Shell;
-using Microsoft.Phone.Info;
+    using System.Collections.Generic;
+    using System.IO.IsolatedStorage;
+    using System.Linq;
+    using System.Runtime.Serialization;
+    using System.Windows;
+    using System.Windows.Media;
+    using Windows.Graphics.Display;
+    using Microsoft.Phone.Controls;
+    using Microsoft.Phone.Shell;
+    using Microsoft.Phone.Info;
+    using WPCordovaClassLib.Cordova;
+    using WPCordovaClassLib.Cordova.Commands;
+    using WPCordovaClassLib.Cordova.JSON;
 
 
-
-using System.Threading.Tasks;
-using Windows.ApplicationModel.DataTransfer;
-using Windows.UI.Core;
-using Windows.ApplicationModel;
-using Microsoft.Phone.Tasks;
-
-using WPCordovaClassLib.Cordova;
-using WPCordovaClassLib.Cordova.Commands;
-using WPCordovaClassLib.Cordova.JSON;
-
-
-    
-
-
-
-    
     
 namespace Cordova.Extension.Commands
 {
